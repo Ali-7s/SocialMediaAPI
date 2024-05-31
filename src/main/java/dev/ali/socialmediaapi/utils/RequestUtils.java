56df6dd703852b281b46ec09b509debcf6d0acb6
Ali Sharif <alisharif29@gmail.com>
2024-05-30 17:39:33 -0700

    Update getResponse method

--- src/main/java/dev/ali/socialmediaapi/utils/RequestUtils.java
+++ src/main/java/dev/ali/socialmediaapi/utils/RequestUtils.java
@@ -1,17 +1,12 @@
 package dev.ali.socialmediaapi.utils;
 
 import dev.ali.socialmediaapi.model.ApiResponse;
-import org.springframework.http.HttpStatus;
-
 import java.time.LocalDateTime;
 import java.util.Map;
-
-import static org.apache.commons.lang3.StringUtils.EMPTY;
-
 public class RequestUtils {
 
-    public static ApiResponse getResponse(Map<?, ?> data, String message, HttpStatus status) {
-        return new ApiResponse(LocalDateTime.now().toString(), status.value(), HttpStatus.valueOf(status.value()), message, EMPTY, data);
+    public static ApiResponse getResponse(Map<?, ?> data, String message) {
+        return new ApiResponse(LocalDateTime.now().toString(), message, data);
     }
 
 }
