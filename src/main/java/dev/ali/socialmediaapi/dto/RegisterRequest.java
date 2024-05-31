090a1881930dee2a2aa3dfe0755d06655c62ec78
Ali Sharif <alisharif29@gmail.com>
2024-05-30 17:22:01 -0700

    Added validation constraints to RegisterRequest.java

--- src/main/java/dev/ali/socialmediaapi/dto/RegisterRequest.java
+++ src/main/java/dev/ali/socialmediaapi/dto/RegisterRequest.java
@@ -1,4 +1,15 @@
 package dev.ali.socialmediaapi.dto;
+import jakarta.validation.constraints.*;
 
-public record RegisterRequest(String email, String username, String displayName, String password) {
-}
+public record RegisterRequest(
+        @NotEmpty(message = "Email cannot be null")
+        @Email(message = "Please enter a valid email address")
+        String email,
+        @Pattern(regexp = "^[A-Za-z0-9_]{1,15}$", message = "Invalid username. Please use only letters (a-z), numbers, and underscores.")
+        String username,
+        @NotEmpty(message = "Display name cannot be null")
+        @Size(min= 1, max = 55, message = "Display name must be between 1-55 characters")
+        String displayName,
+        @NotNull(message = "Password cannot be null")
+        @Size(min = 8, message = "Password length must be 8 characters or more")
+        String password) { }
