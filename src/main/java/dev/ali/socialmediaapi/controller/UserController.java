48264f905b50e545b1e2ae525891e6605b2224f6
Ali Sharif <alisharif29@gmail.com>
2024-06-13 19:59:42 -0700

    Added user controller methods for following and unfollowing

--- src/main/java/dev/ali/socialmediaapi/controller/UserController.java
+++ src/main/java/dev/ali/socialmediaapi/controller/UserController.java
@@ -6,9 +6,11 @@ import dev.ali.socialmediaapi.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
 import static dev.ali.socialmediaapi.utils.RequestUtils.getResponse;
@@ -42,5 +44,27 @@ public class UserController {
     @GetMapping("/search/{username}")
     public ResponseEntity<ApiResponse> findByUsername(@PathVariable String username) {
         return new ResponseEntity<>(getResponse(Map.of("user", userService.findByUsername(username)), "User successfully retrieved."), OK);
+    }
+
+    @PostMapping("/{followerId}/follow/{followingId}")
+    public ResponseEntity<ApiResponse> followUser(@PathVariable Long followerId, @PathVariable Long followingId) {
+        userService.followUser(followerId, followingId);
+        return new ResponseEntity<>(getResponse(Collections.emptyMap(), "User successfully followed."), OK);
+    }
+
+    @PostMapping("/{followerId}/unfollow/{followingId}")
+    public ResponseEntity<ApiResponse> unfollowUser(@PathVariable Long followerId, @PathVariable Long followingId) {
+        userService.unfollowUser(followerId, followingId);
+        return new ResponseEntity<>(getResponse(Collections.emptyMap(), "User successfully unfollowed."), OK);
+    }
+
+    @GetMapping("/{id}/followers")
+    public ResponseEntity<ApiResponse> getFollowers(@PathVariable Long id) {
+        return new ResponseEntity<>(getResponse(Map.of("followers", userService.getFollowers(id)), "User followers successfully retrieved."), OK);
+    }
+
+    @GetMapping("/{id}/following")
+    public ResponseEntity<ApiResponse> getFollowing(@PathVariable Long id) {
+        return new ResponseEntity<>(getResponse(Map.of("following", userService.getFollowing(id)), "User following successfully retrieved."), OK);
     }
 }
