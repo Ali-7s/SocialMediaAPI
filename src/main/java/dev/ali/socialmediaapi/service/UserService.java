08e57c4417ea4ffef7b5a44fad0d68ca71e4a2e5
Ali Sharif <alisharif29@gmail.com>
2024-06-13 19:57:59 -0700

    Added UserService methods for following and unfollowing user

--- src/main/java/dev/ali/socialmediaapi/service/UserService.java
+++ src/main/java/dev/ali/socialmediaapi/service/UserService.java
@@ -2,10 +2,12 @@ package dev.ali.socialmediaapi.service;
 
 import dev.ali.socialmediaapi.model.User;
 import dev.ali.socialmediaapi.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class UserService {
